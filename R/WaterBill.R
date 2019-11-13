bd76a5735e4e7f4093f9e142838f142ef8cf5672
Bjorn Kallerud <bjornkallerud@gmail.com>
2019-11-13 07:03:19 -0800

    update

--- R/WaterBill.R
+++ R/WaterBill.R
@@ -9,7 +9,7 @@
 #'
 #' @param df \code{dataframe} that contains customer usage - var must be titled \code{usage}. If rates are not uniform across
 #' customers, fixed rate columns must be titled \code{fixed_c} and \code{fixed_p}
-#' @param elasticity the elasticity parameter, default is set to zero which excludes elasticity from the analysis
+#' @param elasticity_par the elasticity parameter, default is set to zero which excludes elasticity from the analysis
 #' @param nt_c number of tiers in the current rate structure
 #' @param nt_p number of tiers in the proposed rate structure
 #' @param uniform_rates \code{logical} if \code{TRUE} the rates are constant across all customers in dataframe. If \code{TRUE}
@@ -31,7 +31,8 @@
 #'
 #' @export
 WaterBill <- function(df, uniform_rates = TRUE, nt_c, wdths_c = NULL, fxd_c = NULL, vol_c = NULL,
-                      proposed_rates = TRUE, elasticity = 0.0, nt_p = NULL, wdths_p = NULL, fxd_p = NULL,  vol_p = NULL) {
+                      proposed_rates = TRUE, elasticity_par = 0.0, elasticity_response = "average",
+                      nt_p = NULL, wdths_p = NULL, fxd_p = NULL,  vol_p = NULL) {
 
   # Define columns dynamically
   use_cols_current <- paste0("t", 1:nt_c, "_use_current")
@@ -67,6 +68,16 @@ WaterBill <- function(df, uniform_rates = TRUE, nt_c, wdths_c = NULL, fxd_c = NU
 
   }
 
+  # Include rate cols
+  df[rate_cols_current] <- vol_c
+  df[rate_cols_proposed] <- vol_p
+
+  # Include fixed charges
+  df$fixed_current <- fxd_c
+  df$fixed_proposed <- fxd_p
+  df$fixed_prime <- fxd_p
+
+
   # Calculate current charges based on tiered use
   df[charge_cols_current] <- if (uniform_rates == TRUE) {df[use_cols_current] * vol_c} else {
     df[use_cols_current] * df[rate_cols_current]}
@@ -80,19 +91,33 @@ WaterBill <- function(df, uniform_rates = TRUE, nt_c, wdths_c = NULL, fxd_c = NU
 
   # Calculate total bill
   df <- mutate(df, bill_current = if (uniform_rates == TRUE) {fxd_c + rowSums(df[charge_cols_current])} else {
-    fixed_c + rowSums(df[charge_cols_current])})
+    fxd_c + rowSums(df[charge_cols_current])})
 
   if (proposed_rates == TRUE) {
 
     df <- df %>%
-      mutate(bill_proposed = if (uniform_rates == T) {fxd_c + rowSums(df[charge_cols_proposed])} else {
-        fixed_p + rowSums(df[charge_cols_proposed])},
+
+      mutate(bill_proposed = if (uniform_rates == T) {fxd_p + rowSums(df[charge_cols_proposed])} else {
+        fxd_p + rowSums(df[charge_cols_proposed])},
         avg_price_current = ifelse(is.infinite(bill_current / usage), 99999999, bill_current / usage),
-        avg_price_proposed = ifelse(is.infinite(bill_proposed / usage), 99999999, bill_proposed / usage),
+        avg_price_proposed = ifelse(is.infinite(bill_proposed / usage), 99999999, bill_proposed / usage)) %>%
 
-        usage_prime = usage + elasticity * (avg_price_proposed - avg_price_current) / avg_price_current * usage)
+      mutate(
 
-    # Allocate usage prime into tiers
+        marginal_price_current = if (nt_c == 1) {t1rate_current} else if (nt_c == 2) {t2rate_current} else if (nt_c == 3) {
+          t3rate_current} else if (nt_c == 4) {t4rate_current} else if (nt_c == 5) {t5rate_current} else if (nt_c == 6) {t6rate_current},
+
+        marginal_price_proposed = if (nt_p == 1) {t1rate_proposed} else if (nt_p == 2) {t2rate_proposed} else if (nt_p == 3) {
+          t3rate_proposed} else if (nt_p == 4) {t4rate_proposed} else if (nt_p == 5) {t5rate_proposed} else if (nt_p == 6) {t6rate_proposed}) %>%
+
+      mutate(
+        usage_prime = if (elasticity_response == "average") {usage + (elasticity_par * (avg_price_proposed - avg_price_current) / avg_price_current) * usage} else if (elasticity_response == "marginal") {
+          usage + (elasticity_par * (marginal_price_proposed - marginal_price_current) / marginal_price_current) * usage
+        }
+      )
+
+
+    #Allocate usage prime into tiers
     df[use_cols_prime] <- do.call("TierConsumption", c(list(nt_p), list(df$usage_prime),
                                                        if (uniform_rates == T) {unname(wdths_p)} else {
                                                          unname(df[width_cols_proposed])}))
@@ -105,11 +130,61 @@ WaterBill <- function(df, uniform_rates = TRUE, nt_c, wdths_c = NULL, fxd_c = NU
         fixed_p + rowSums(df[charge_cols_prime])},
 
         perc_change_bill = (bill_prime - bill_current) / bill_current
-      )
+
+        )
 
   }
 
   return(df)
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
