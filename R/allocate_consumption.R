3a2b83617511c83d2c758de576987ab7c6e897df
Bjorn Kallerud <bjornkallerud@gmail.com>
2020-04-17 16:52:58 -0700

    calc_bill fxn

--- R/allocate_consumption.R
+++ R/allocate_consumption.R
@@ -1,7 +1,7 @@
 ###########################################################################################################################################################
 #' Allocate Tiered Water Consumption
 #'
-#' Distributes a customers total water consumption over various tiers. Allows for heterogeneous rate structures.
+#' Distributes a customers total water consumption over various tiers.
 #'
 #' @param df dataframe that contains customer usage and tier widths. usage column can be titled either "use" or "usage". width columns titles must take the form of "tX_width".
 #' @param suffix width column suffix. we often define widths in terms of current/proposed rates (ie tX_width_current), so defining the suffix enables use of this function for current and proposed widths.
@@ -15,10 +15,10 @@
 #' @export
 #'
 
-allocate_consumption <- function(df, suffix = NULL) {
+allocate_consumption <- function(df, suffix = NULL, use.prime = FALSE) {
 
   # If there is a suffix (ie proposed or current widths) then we want to drop the suffix, conduct the manipulations and then paste in the suffix
-  if (! is.null(suffix)) {names(df) <- gsub(suffix, "", names(df))}
+  # if (! is.null(suffix)) {names(df) <- gsub(suffix, "", names(df))}
 
   # Attach dataframe so we can utilize exists()
   attach(df)
@@ -27,30 +27,45 @@ allocate_consumption <- function(df, suffix = NULL) {
   kUsage <- (exists("usage"))
   df <- df %>% {if(exists("usage")) rename(., use = usage) else .}
 
+  # We calculate use_prime when incorporating elasticity, need a way to have two use columns
+  # Method - rename actual use as temporary name and then change prime to use, run calculations, change names back
+  if (use.prime){
+    df <- df %>%
+      rename(use_tmp = use,
+             use = use_prime)
+  }
+
   # Define tier maximums
   df <- df %>%
-    {if(exists("t1_width")) mutate(., t1_max = t1_width)           else mutate(., t1_max = 9999999999)} %>%
-    {if(exists("t2_width")) mutate(., t2_max = t1_max + t2_width)  else mutate(., t2_max = 9999999999)} %>%
-    {if(exists("t3_width")) mutate(., t3_max = t2_max + t3_width)  else mutate(., t3_max = 9999999999)} %>%
-    {if(exists("t4_width")) mutate(., t4_max = t3_max + t4_width)  else mutate(., t4_max = 9999999999)} %>%
-    {if(exists("t5_width")) mutate(., t5_max = t4_max + t5_width)  else mutate(., t5_max = 9999999999)}
+    {if(exists(paste0("t1_width", suffix))) mutate(., t1_max = get(paste0("t1_width", suffix)))           else mutate(., t1_max = 9999999999)} %>%
+    {if(exists(paste0("t2_width", suffix))) mutate(., t2_max = t1_max + get(paste0("t2_width", suffix)))  else mutate(., t2_max = 9999999999)} %>%
+    {if(exists(paste0("t3_width", suffix))) mutate(., t3_max = t2_max + get(paste0("t3_width", suffix)))  else mutate(., t3_max = 9999999999)} %>%
+    {if(exists(paste0("t4_width", suffix))) mutate(., t4_max = t3_max + get(paste0("t4_width", suffix)))  else mutate(., t4_max = 9999999999)} %>%
+    {if(exists(paste0("t5_width", suffix))) mutate(., t5_max = t4_max + get(paste0("t5_width", suffix)))  else mutate(., t5_max = 9999999999)}
 
   # Allocate usage
   df <- df %>%
-    {if(exists("t1_width")) mutate(., t1_use = ifelse(use > t1_max, t1_max, use),
+    {if(exists(paste0("t1_width", suffix))) mutate(., t1_use = ifelse(use > t1_max, t1_max, use),
                                    t2_use = ifelse(use > t1_max, ifelse(use >= t2_max, t2_max - t1_max, use - t1_max), 0)) else
                                      mutate(., t1_use = use)} %>%
-    {if(exists("t2_width")) mutate(., t3_use = ifelse(use > t1_max, ifelse(use >= t2_max, t2_max - t1_max, use - t1_max), 0)) else .} %>%
-    {if(exists("t3_width")) mutate(., t4_use = ifelse(use > t2_max, ifelse(use >= t3_max, t3_max - t2_max, use - t2_max), 0)) else .} %>%
-    {if(exists("t4_width")) mutate(., t5_use = ifelse(use > t3_max, ifelse(use >= t4_max, t4_max - t3_max, use - t3_max), 0)) else .} %>%
-    {if(exists("t5_width")) mutate(., t6_use = ifelse(use > t4_max, ifelse(use >= t5_max, t5_max - t4_max, use - t4_max), 0)) else .}
+    {if(exists(paste0("t2_width", suffix))) mutate(., t3_use = ifelse(use > t2_max, ifelse(use > t3_max, t3_max - t2_max, use - t2_max), 0)) else .} %>%
+    {if(exists(paste0("t3_width", suffix))) mutate(., t4_use = ifelse(use > t3_max, ifelse(use > t4_max, t4_max - t3_max, use - t3_max), 0)) else .} %>%
+    {if(exists(paste0("t4_width", suffix))) mutate(., t5_use = ifelse(use > t4_max, ifelse(use > t5_max, t5_max - t4_max, use - t4_max), 0)) else .} %>%
+    {if(exists(paste0("t5_width", suffix))) mutate(., t6_use = ifelse(use > t5_max, use - t5_max, 0)) else .}
 
   # Drop maximums
   df <- df %>% select(., - contains("max"))
 
   # Tweak names - change use to usage and add suffix if need be
-  if (! is.null(suffix)) {names(df) <- gsub("_use", paste0("_use", suffix), names(df))}
+  if (! is.null(suffix)) {names(df) <- gsub("_use$", paste0("_use", ifelse(use.prime == F, suffix, "_prime")), names(df))}
   if (kUsage) {df <- rename(df, usage = use )}
+
+  # Switch names back if prime
+  if (use.prime){
+    df <- df %>%
+      rename(use_prime = use,
+             use = use_tmp)
+  }
 
   detach(df)
   return(df)
---
+++ R/calculate_bill.R
@@ -0,0 +1,87 @@
+###########################################################################################################################################################
+#' Calculate Water Bill
+#'
+#' Calculates customers' water bills based on fixed and volumetric rates. There is an option to include both current
+#' and proposed rate structures to assess bill impacts.
+#'
+#' BillCalculation <- function(df, elasticity = 0.0, nt_c, nt_p = NULL, uniform_rates = TRUE, proposed_rates = TRUE,
+#wdths_c = NULL, wdths_p = NULL, fxd_c = NULL, fxd_p = NULL, vol_c = NULL, vol_p = NULL)
+#'
+#' @param df_use \code{dataframe} that contains customer usage - var must be titled \code{usage} or \code{use}.
+#' @param df_rates \code{dataframe} that contains current and proposed fixed and volumetric rates.
+#' @param elasticity_par the elasticity parameter, default is set to zero which excludes elasticity from the analysis.
+#'
+#' @return This function returns a \code{dataframe} that includes all final bills under the current and proposed rate
+#' structures as well as their respective tier components.
+#'
+#' @author Bjorn Kallerud
+#'
+#' @import dplyr
+#'
+#' @export
+#'
+
+# set.seed(8)
+# df_use <- data.frame(account_number = 1:10, meter_size = c("0.75\"", "1\""), use = round(runif(10, 10, 30), 1), class = "sfr")
+# df_rates <- data.frame(meter_size = c("0.75\"", "1\""), class = "sfr",
+#                        t1_width_current = c(10, 11),
+#                        t1_width_proposed = c(10, 11), t2_width_proposed = c(5, 6),
+#                        fixed_current = c(20, 30), fixed_proposed = c(21, 32),
+#                        t1_rate_current = 2, t2_rate_current = 3, t1_rate_proposed = 1.9, t2_rate_proposed = 3, t3_rate_proposed = 5)
+
+calculate_bill <- function(df_use, df_rates, rate_group = c("class", "meter_size"), elasticity_par = 0.0, elasticity_response = "average") {
+
+  # Store number of tiers for current and proposed rates
+  nt_c <- ifelse(is.null(df_rates), length(grep("rate_c", colnames(df_use))), length(grep("rate_c", colnames(df_rates))))
+  nt_p <- ifelse(is.null(df_rates), length(grep("rate_p", colnames(df_use))), length(grep("rate_p", colnames(df_rates))))
+
+  # Join use and rate dataframes
+  if (!is.null(df_rates)) {
+    df <- left_join(df_use, df_rates, by = rate_group)
+  } else {df <- df_use}
+  #TODO include warning if not perfect match
+
+  ## Group columns for calculations
+  # Current
+  use_cols_current    <- paste0("t", 1:nt_c, "_use_current")
+  width_cols_current  <- paste0("t", 1:(nt_c - 1), "_width_current")
+  rate_cols_current   <- paste0("t", 1:nt_c, "_rate_current")
+  charge_cols_current <- paste0("t", 1:nt_c, "_charges_current")
+  # Proposed
+  use_cols_proposed    <- paste0("t", 1:nt_p, "_use_proposed")
+  width_cols_proposed  <- paste0("t", 1:(nt_p - 1), "_width_proposed")
+  rate_cols_proposed   <- paste0("t", 1:nt_p, "_rate_proposed")
+  charge_cols_proposed <- paste0("t", 1:nt_p, "_charges_proposed")
+  # Prime - accounts for elasticity
+  use_cols_prime    <- paste0("t", 1:nt_p, "_use_prime")
+  charge_cols_prime <- paste0("t", 1:nt_p, "charges_prime")
+
+  # Allocate consumption
+  df <- df %>%
+    allocate_consumption(suffix = "_current") %>%
+    allocate_consumption(suffix = "_proposed")
+
+  # Calculate charges
+  df[charge_cols_current]  <- df[use_cols_current] * df[rate_cols_current]
+  df[charge_cols_proposed] <- df[use_cols_proposed] * df[rate_cols_proposed]
+
+  # Calculate total bill
+  df <- df %>%
+    mutate(bill_current  = fixed_current  + rowSums(df[charge_cols_current ])) %>%
+    mutate(bill_proposed = fixed_proposed + rowSums(df[charge_cols_proposed])) %>%
+    mutate(avg_price_current  = ifelse(is.infinite(bill_current  / use), 99999999, bill_current  / use)) %>%
+    mutate(avg_price_proposed = ifelse(is.infinite(bill_proposed / use), 99999999, bill_proposed / use)) %>%
+    # Calculate use that incorporates elasticity
+    mutate(use_prime = use + (elasticity_par * (avg_price_proposed - avg_price_current) / avg_price_current) * use) %>%
+    # Allocate usage prime into tiers and calculate bill
+    allocate_consumption(suffix = "_proposed", use.prime = T)
+
+  df[charge_cols_prime] <- df[use_cols_prime] * df[rate_cols_proposed]
+
+  df <- df %>%
+    mutate(bill_prime = fixed_proposed + rowSums(df[charge_cols_prime]),
+           perc_change_bill = (bill_prime - bill_current) / bill_current)
+
+  return(df)
+
+}
